3d25bb299dc9f92cb5e81889507f91d9699b05e7
Patrick Schafer <pschafer@digipen.edu>
2012-12-19 13:04:24 -0800

    Working app

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 var express = require('express');
-// var app = express();
-var app = express.createServer(express.logger());
+var socket = require('socket.io');
+var app = express.createServer();
+var io = socket.listen(app);
 
-var io = require('socket.io').listen(app);
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
 
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on('connection', function(client) {
+  client.on('messages', function(data) {
+    client.broadcast.emit('messages', data);
+  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
 });
-
-app.get('/', function(request, response) {
-  io.sockets.on('connection', function() {
-    console.log('Client connected');
-  });
-});
-
-// var http = require('http');
-// var express = require('express');
-// var app = express();
-// app = http.createServer(app);
