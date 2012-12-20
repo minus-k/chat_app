2e2d97a511f755926ce66905b6f793dffc8e6529
Patrick Schafer <pschafer@digipen.edu>
2012-12-20 08:16:24 -0800

    Added basic stylesheet and formatting

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.sendfile(__dirname + "/index.html");
 });
 
+
+app.get('/style.css', function(request, response) {
+  response.sendfile(__dirname + "/style.css");
+});
+
 var messages = [];
 var store_message = function(name, data) {
   messages.push({name: name, data: data});
@@ -23,10 +28,8 @@ var store_message = function(name, data) {
 
 io.sockets.on('connection', function(client) {
 
-
   client.on('join', function(name) {
     client.set('nickname', name);
-    client.broadcast.emit('chat', name + ' has joined the chat');
     messages.forEach(function(message) {
       client.emit('messages', message.name + ': ' + message.data);
     });
@@ -36,12 +39,6 @@ io.sockets.on('connection', function(client) {
     client.get('nickname', function(error, name) {
       store_message(name, message);
       client.broadcast.emit("messages", name + ": " + message);
-    });
-  });
-
-  client.on('disconnect', function() {
-    client.get('nickname', function(error, name) {
-      client.broadcast.emit('left', name + ' has left the chat');
     });
   });
 });
