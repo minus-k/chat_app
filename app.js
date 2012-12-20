8724843ff53804ed089f0f1196400b668543bfd8
Patrick Schafer <pschafer@digipen.edu>
2012-12-19 17:43:58 -0800

    Added in-memory chat storage

--- app.js
+++ app.js
@@ -13,14 +13,35 @@ app.get('/', function(request, response) {
   response.sendfile(__dirname + "/index.html");
 });
 
+var messages = [];
+var store_message = function(name, data) {
+  messages.push({name: name, data: data});
+  if (messages.length > 15) {
+    messages.shift();
+  }
+}
+
 io.sockets.on('connection', function(client) {
+
+
   client.on('join', function(name) {
     client.set('nickname', name);
+    client.broadcast.emit('chat', name + ' has joined the chat');
+    messages.forEach(function(message) {
+      client.emit('messages', message.name + ': ' + message.data);
+    });
   });
 
   client.on('messages', function(message) {
     client.get('nickname', function(error, name) {
+      store_message(name, message);
       client.broadcast.emit("messages", name + ": " + message);
+    });
+  });
+
+  client.on('disconnect', function() {
+    client.get('nickname', function(error, name) {
+      client.broadcast.emit('left', name + ' has left the chat');
     });
   });
 });
